ca3a8573462fd5e3513ddc0af3c771d07764039e
taniot <gaetano.frascolla@gmail.com>
2022-04-07 20:16:18 +0200

    fix: canonical

--- components/layout/LayoutHome.component.tsx
+++ components/layout/LayoutHome.component.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Head from 'next/head';
-import Footer from '../footer/footer.component';
-import Cover from '../cover/cover.component';
-import WhatsApp from '../whatsapp/whatsapp.component';
+import React from "react";
+import Head from "next/head";
+import Footer from "../footer/footer.component";
+import Cover from "../cover/cover.component";
+import WhatsApp from "../whatsapp/whatsapp.component";
 
 type Props = {
   title?: string;
@@ -10,18 +10,18 @@ type Props = {
 };
 
 const defaultTitle =
-  'Sa Marchesa - Fiore Sardo e pecorini a latte crudo e naturali';
+  "Sa Marchesa - Fiore Sardo e pecorini a latte crudo e naturali";
 
 const LayoutHome = ({ children, title }: Props): JSX.Element => (
-  <div className='layout-home'>
+  <div className="layout-home">
     <Head>
       <title>{title ? `${title} | ${defaultTitle}` : defaultTitle}</title>
       <meta
-        name='description'
+        name="description"
         content={`Sa Marchesa è l'azienda agricola del pastore Giuseppe Cugusi, che produce Fiore Sardo e pecorini naturali a latte crudo nell'altipiano di Paulilatino, in Sardegna.`}
       />
-      <link rel='canonical' href='https://samarchesa.com' />
-      <link rel='icon' href='/favicon.png' />
+      <link rel="canonical" href="https://www.samarchesa.com" />
+      <link rel="icon" href="/favicon.png" />
     </Head>
     <WhatsApp />
     <Cover />
